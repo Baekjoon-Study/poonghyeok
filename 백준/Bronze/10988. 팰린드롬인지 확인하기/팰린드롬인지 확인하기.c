2e5ad37cd48cb517aa3ffa355f7b975888844d1b
dlvndgur <165930898+dlvndgur@users.noreply.github.com>
2024-08-11 21:48:08 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char word[101];
+    scanf("%s", word);
+    int len = strlen(word);
+    int result = 1;
+
+    for(int i=0; i<len/2; i++){
+        if (word[i] != word[len-1-i]){
+            result = 0;
+            break;
+        }
+    }
+    printf("%d", result);
+    return 0;
+}
+
+
